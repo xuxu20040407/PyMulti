9c74031b4eddbd6537128d1afe8e6f9001cb429b
xuxu20040407 <wangzixu20040407@163.com>
2024-11-14 20:12:07 +0800

    update

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ new_dir = pm.init1D('Multi-1D')
 pm.generate_input_data1D(new_dir,0, None, None, None, None)
 pm.run_command_1D(new_dir,0)
 
-new_dir = pm.init2D('Multi-2D')
+new_dir = pm.init2D('Multi-2D',0)
 pm.generate_input_data2D(new_dir,0, None, None, None, None)
 pm.run_command_2D(new_dir,0)
