d9def22e00b5b4362cf036286d1898aac0697f8d
xuxu20040407 <wangzixu20040407@163.com>
2024-11-14 20:22:01 +0800

    update

--- 2D_database.py
+++ 2D_database.py
@@ -13,14 +13,14 @@ Targets_grid = sampler.uniform_sampling(Targets_range_max, Targets_range_min, n_
 # Laser_power_range_max=[2,2,5,40,300]
 # Laser_time = [0,2,2,2,2,2,2,0]
 # Laser_grid = sampler.uniform_sampling([0.17, 0.003, 0.02], [0.15, 0.0015, 0.015], n_splits)
-program_name = 'Multi-1D'
+program_name = 'Multi-2D'
 
 def process_task(index, new_dir):
-    pm.generate_input_data1D(new_dir, index, None, Targets_grid[index, 0], Targets_grid[index, 1], Targets_grid[index, 2])
+    pm.generate_input_data2D(new_dir, index, None, Targets_grid[index, 0], Targets_grid[index, 1], Targets_grid[index, 2])
     pm.run_command_2D(new_dir, index)
 
 if __name__ == '__main__':
-    new_dir = pm.init2D(program_name)
+    new_dir = pm.init2D(program_name,n_splits**3)
     with Pool(processes=10) as pool:
         # 使用 functools.partial 来传递 new_dir 参数
         from functools import partial
